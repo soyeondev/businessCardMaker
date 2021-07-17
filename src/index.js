6570750c261bcf930d2dab99d3256f909abcce84
unknown <soyeon.dev@gmail.com>
2021-07-17 23:29:57 +0900

    add login and logout functionality

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import AuthService from './service/auth_service';
 
 const authService = new AuthService();
 
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
-console.log(process.env.REACT_APP_FIREBASE_DOMAIN);
-console.log(process.env.REACT_APP_FIREBASE_FIREBASE_PROJECT_ID);
-console.log(process.env.REACT_APP_FIREBASE_FIREBASE_DB_URL);
-
 ReactDOM.render(
   <React.StrictMode>
     <App authService={authService}/>
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -6,6 +6,18 @@ class AuthService {
         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
         return firebaseApp.auth().signInWithPopup(authProvider);
     }
+
+    logout() {
+        return firebaseApp.auth().signOut();
+    }
+
+    onAuthChange(onUserChanged) {
+        console.log("onUserChanged: ", onUserChanged);
+        firebase.auth().onAuthStateChanged(user => {
+            console.log("user1: ", user);
+            onUserChanged(user);
+        })
+    }
 }
 
 export default AuthService;
