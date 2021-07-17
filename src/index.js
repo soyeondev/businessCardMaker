c58e645a02d62fa0fbd8ec88cdd1b6ff5b1afd8a
unknown <soyeon.dev@gmail.com>
2021-07-17 22:12:17 +0900

    add login component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './app';
+import AuthService from './service/auth_service';
+
+const authService = new AuthService();
+
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+console.log(process.env.REACT_APP_FIREBASE_DOMAIN);
+console.log(process.env.REACT_APP_FIREBASE_FIREBASE_PROJECT_ID);
+console.log(process.env.REACT_APP_FIREBASE_FIREBASE_DB_URL);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App authService={authService}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
