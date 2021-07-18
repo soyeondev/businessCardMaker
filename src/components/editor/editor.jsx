146b5a94e3e7825e5e012fa42d29259277fa6cc9
unknown <soyeon.dev@gmail.com>
2021-07-18 13:03:17 +0900

    add card and edit form components

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import styles from './editor.module.css';
+import React from "react";
+import Card from "../card/card";
+import CardEditForm from "../card_edit/card_edit_form";
+import styles from "./editor.module.css";
 
-const Editor = props => (
-    <section className={styles.editor}>
-        <h1 className={styles.title}>Card Maker</h1>
-    </section>
+const Editor = ({ cards }) => (
+  <section className={styles.editor}>
+    <h1 className={styles.title}>Card Maker</h1>
+    {cards.map((card) => (
+      <CardEditForm card={card} />
+    ))}
+  </section>
 );
 
 export default Editor;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,35 +1,69 @@
-import React, { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import Editor from '../editor/editor';
-import Footer from '../footer/footer';
-import Header from '../header/header';
-import Preview from '../preview/preview';
-import styles from './maker.module.css';
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Editor from "../editor/editor";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import Preview from "../preview/preview";
+import styles from "./maker.module.css";
 
-const Maker = ({authService}) => {
-     const history = useHistory();
-     const onLogout = () => {
-          authService.logout();
-     };
+const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "angela1",
+      company: "Company",
+      theme: "light",
+      title: "Software Engineer",
+      email: "abc@gmail.com",
+      message: "go for it",
+      fileName: "abc",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "angela2",
+      company: "Company",
+      theme: "dark",
+      title: "Software Engineer",
+      email: "abc@gmail.com",
+      message: "go for it",
+      fileName: "abc",
+      fileURL: "ellie.png",
+    },
+    {
+      id: "3",
+      name: "angela3",
+      company: "Company",
+      theme: "colorful",
+      title: "Software Engineer",
+      email: "abc@gmail.com",
+      message: "go for it",
+      fileName: "abc",
+      fileURL: null,
+    },
+  ]);
+  const history = useHistory();
+  const onLogout = () => {
+    authService.logout();
+  };
 
-     useEffect(() => {
-          authService
-          .onAuthChange(user => {
-               if(!user){
-                    history.push('/');
-               }
-          });
-     });
-     return (
-          <section className={styles.maker}>
-               <Header onLogout={onLogout}/>
-               <div className={styles.container}>
-                    <Editor/>
-                    <Preview/>
-               </div>
-               <Footer/>
-          </section>     
-     );
-}
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        history.push("/");
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor cards={cards} />
+        <Preview cards={cards} />
+      </div>
+      <Footer />
+    </section>
+  );
+};
 
 export default Maker;
