951ae31460730d1a23a21b9cf7093cf53679c1aa
unknown <soyeon.dev@gmail.com>
2021-07-18 14:36:16 +0900

    add CardAddForm component

---
+++ src/components/card_add/card_add_form.jsx
@@ -0,0 +1,80 @@
+import React, { useRef } from "react";
+import Button from "../button/button";
+import ImageFileInput from "../image_file_input/image_file_input";
+import styles from "./card_add_form.module.css";
+
+const CardAddForm = ({ onAdd }) => {
+  const formRef = useRef();
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const messageRef = useRef();
+  const emailRef = useRef();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const card = {
+      id: Date.now(), //uuid
+      name: nameRef.current.value || "",
+      company: companyRef.current.value || "",
+      theme: themeRef.current.value || "",
+      title: titleRef.current.value || "",
+      message: messageRef.current.value || "",
+      email: emailRef.current.value || "",
+      fileName: "",
+      fileURL: "",
+    };
+    formRef.current.reset();
+    onAdd(card);
+  };
+  return (
+    <form ref={formRef} className={styles.form}>
+      <input
+        ref={nameRef}
+        className={styles.input}
+        type="text"
+        name="name"
+        placeholder="name"
+      />
+      <input
+        ref={companyRef}
+        className={styles.input}
+        type="text"
+        name="company"
+        placeholder="company"
+      />
+      <select ref={themeRef} name="theme" className={styles.select}>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input
+        ref={titleRef}
+        className={styles.input}
+        type="text"
+        name="title"
+        placeholder="title"
+      />
+      <input
+        ref={emailRef}
+        className={styles.input}
+        type="text"
+        name="email"
+        placeholder="email"
+      />
+      <textarea
+        ref={messageRef}
+        className={styles.textarea}
+        name="message"
+        placeholder="message"
+      />
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name="Add" onClick={onSubmit} />
+    </form>
+  );
+};
+
+export default CardAddForm;
